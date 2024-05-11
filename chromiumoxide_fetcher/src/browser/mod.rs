b5fc2534cae62cc4ecc019198c17ce1fff136fe9
Ankur Srivastava <best.ankur@gmail.com>
2024-05-11 23:08:08 +0200

    feat: make BrowserFetcherRuntime public in order to be used for linux-aarch64

--- chromiumoxide_fetcher/src/browser/mod.rs
+++ chromiumoxide_fetcher/src/browser/mod.rs
@@ -1,7 +1,7 @@
 pub use self::fetcher::BrowserFetcher;
 pub use self::options::BrowserFetcherOptions;
 pub use self::revision_info::BrowserFetcherRevisionInfo;
-use self::runtime::BrowserFetcherRuntime;
+pub use self::runtime::BrowserFetcherRuntime;
 use self::zip::ZipArchive;
 
 mod fetcher;
