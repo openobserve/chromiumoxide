877ce9dc30084f4212065f9c4258e8bc80c82f97
Subhradeep Chakraborty <chakrabortysubhradeep556@gmail.com>
2024-09-18 12:50:59 +0530

    fix: update fetcher chromium revision to r1336433

--- chromiumoxide_fetcher/src/lib.rs
+++ chromiumoxide_fetcher/src/lib.rs
@@ -4,7 +4,7 @@ pub use self::platform::Platform;
 pub use self::revision::Revision;
 
 /// Currently downloaded chromium revision
-pub const CURRENT_REVISION: Revision = Revision(1045629);
+pub const CURRENT_REVISION: Revision = Revision(1336433);
 
 mod browser;
 mod error;
